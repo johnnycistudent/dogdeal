b0037fa361c941d4d2b209544f5b54c8586a003f
Ubuntu <ubuntu@ip-172-31-89-120.ec2.internal>
2019-08-09 13:29:19 +0000

    email reset urls updated

--- dogdeal/settings.py
+++ dogdeal/settings.py
@@ -174,3 +174,9 @@ STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
